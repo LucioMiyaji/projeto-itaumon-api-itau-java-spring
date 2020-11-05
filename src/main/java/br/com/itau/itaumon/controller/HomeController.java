ddae5543b70a7d584a0bb3c33aa1411ca018ccf5
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-11-05 15:35:47 -0300

    home na api

--- src/main/java/br/com/itau/itaumon/controller/HomeController.java
+++ src/main/java/br/com/itau/itaumon/controller/HomeController.java
@@ -8,17 +8,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.itau.itaumon.beans.Alarme;
-import br.com.itau.itaumon.dao.AlarmeDAO;
-
 
 @RestController // Indica que a classe irá responder protocolos HTTP (GET/POST)
 @CrossOrigin("*")
 public class HomeController {
 
-	@Autowired // indica que o gerenciamento do atributo sera feito pelo Spring
-	private AlarmeDAO dao;
-	
 	@GetMapping("/")
 	public String index(){
 		return "{\"mensagem\": \"Bem indo a API de teste\"}"
