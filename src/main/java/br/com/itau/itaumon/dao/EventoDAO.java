3e7b24ff4facdc2c230faaf47593a798043643fd
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-11-05 15:57:43 -0300

    home na api

--- src/main/java/br/com/itau/itaumon/dao/EventoDAO.java
+++ src/main/java/br/com/itau/itaumon/dao/EventoDAO.java
@@ -5,5 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.itau.itaumon.beans.Evento;
 
 public interface EventoDAO extends CrudRepository <Evento, Integer>{
-
+	List<Evento> findByDataBetween(Date inicio, Date fim);
+	
+	@Query(value="SELECT count(id) as total FROM tb_evento where"
+			+ " data >= :inicio and data<= :fim AND alarme_id = :num", nativeQuery = true)
+		long getByTotal(@Param("inicio") Date inicio, @Param("fim") Date fim, @Param("num") int num);
+	
 }
